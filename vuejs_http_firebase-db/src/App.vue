5588cc0b6a55f59b8fe4f276ceebd7ecb05aa511
MidnightRoam <saldatsenka@list.ru>
2022-08-12 13:18:03 +0300

    Work with learn project using firebase database

---
+++ vuejs_http_firebase-db/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <form class="card" @submit.prevent="createPerson">
+      <h2>Работа с базой данных</h2>
+
+      <div class="form-control">
+        <label for="name">Введите имя</label>
+        <input type="text" id="name" v-model.trim="name">
+      </div>
+
+      <button class="btn primary" :disabled="name.length === 0">Создать человека</button>
+    </form>
+    <app-users-list
+        :users="users"
+        @load="loadUsers"
+    ></app-users-list>
+  </div>
+</template>
+
+<script>
+import AppUsersList from "@/components/AppUsersList";
+
+export default {
+  data() {
+    return {
+      name: '',
+      users: []
+    }
+  },
+  methods: {
+    async createPerson() {
+       const response = await fetch('https://vuejs-with-http-learn-default-rtdb.europe-west1.firebasedatabase.app/users.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          firstName: this.name
+        })
+      }) // отправляем данные на сервер
+
+      const firebaseData = await response.json() // ждем получения данных от сервера
+
+      console.log(firebaseData)
+      this.name = ''
+    }
+  },
+  components: { AppUsersList }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuejs_http_firebase-db/src/components/AppUsersList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="users.length != 0">
+    <div class="card" v-for="user in users" :key="user">
+      <h3>{{ user }}</h3>
+    </div>
+  </div>
+  <div class="card center" v-else>
+    <h4>Пользователей пока нет</h4>
+    <button class="btn" @click="$emit('load')">Загрузить список</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppUsersList",
+  emits: ['load'],
+  props: ['users']
+}
+</script>
+
+<style scoped>
+
+</style>
