d72c7207da2c8fe7d9c40ce622673bbea403cb7c
MidnightRoam <saldatsenka@list.ru>
2022-08-19 13:54:32 +0300

    Added an a online banking learning app

---
+++ vuejs_onlineBanking_app/src/layout/AuthLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "AuthLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
