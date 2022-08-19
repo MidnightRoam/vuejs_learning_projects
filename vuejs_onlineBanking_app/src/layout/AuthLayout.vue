cc374fb501ff44d1828f916894dc82f1454326a9
MidnightRoam <saldatsenka@list.ru>
2022-08-19 14:54:18 +0300

    Extended error handling system

--- vuejs_onlineBanking_app/src/layout/AuthLayout.vue
+++ vuejs_onlineBanking_app/src/layout/AuthLayout.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="container">
-    <div class="card">
+      <app-message></app-message>
       <router-view></router-view>
-    </div>
   </div>
 </template>
 
 <script>
-
+import appMessage from "@/components/ui/AppMessage";
 export default {
-  name: "AuthLayout"
+  name: "AuthLayout",
+  components: {appMessage}
 }
 </script>
 
